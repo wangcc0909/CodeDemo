927e14690f157354205c2896d58afd8a8b927c38
admin <1079793182@qq.com>
2019-01-09 11:37:27 +0800

    最后一次提交

--- src/testAnswer/code13/main.go
+++ src/testAnswer/code13/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type treeNode struct {
 	value int
@@ -8,9 +10,10 @@ type treeNode struct {
 	right *treeNode
 }
 
-//   2
-// 3  5
-//1 1 2 3
+//        10
+//   8         12
+// 6   9     7   13
+//1 7 5  11 4 9 11  14
 
 func SumOfLeft(tn *treeNode) int {
 	if tn == nil {
@@ -28,23 +31,47 @@ func SumOfLeft(tn *treeNode) int {
 
 func main() {
 	tn := treeNode{
-		value:2,
+		value:10,
 		left:&treeNode{
-			value:3,
+			value:8,
 			left:&treeNode{
-				value:1,
+				value:6,
+				left:&treeNode{
+					value:1,
+				},
+				right:&treeNode{
+					value:7,
+				},
 			},
 			right:&treeNode{
-				value:1,
+				value:9,
+				left:&treeNode{
+					value:5,
+				},
+				right:&treeNode{
+					value:11,
+				},
 			},
 		},
 		right:&treeNode{
-			value:5,
+			value:12,
 			left:&treeNode{
-				value:2,
+				value:7,
+				left:&treeNode{
+					value:4,
+				},
+				right:&treeNode{
+					value:9,
+				},
 			},
 			right:&treeNode{
-				value:3,
+				value:13,
+				left:&treeNode{
+					value:11,
+				},
+				right:&treeNode{
+					value:14,
+				},
 			},
 		},
 	}
